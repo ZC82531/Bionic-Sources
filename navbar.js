42faf4dd78ab26e589b8fcc7c37f87924364f399
Zakaria Chowdhury <zc@zakarias-air.mynetworksettings.com>
2024-08-08 09:55:11 -0400

    Vue.js updates

---
+++ navbar.js
@@ -0,0 +1,12 @@
+const NavbarComponent = {
+    template: `
+        <div id="navbar_div">
+            <img id="logo" src="images/logo.PNG" alt="Logo">
+            <a href="index.html"><div class="items">Covid</div></a>
+            <a href="chat.html"><div class="items">Chat</div></a>
+            <a href="vaccine.html"><div class="items">Vaccine</div></a>
+            <a href="statistics.html"><div class="items">Statistics</div></a>
+            <a href="variants.html"><div class="items">Variants</div></a>
+        </div>
+    `
+};
